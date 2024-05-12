114262f09b5d78de6bc36dec2758142ba9e6f40a
Frederik Zorn <fz@zorrn.net>
2024-05-12 12:39:55 +0200

    fix(cf): use custom domain for production build

--- build/cf.sh
+++ build/cf.sh
@@ -8,5 +8,9 @@ curl -fsSL https://moonrepo.dev/install/proto.sh | bash -s -- --yes
 source ~/.bash_profile # Reload bash to use `proto` command
 proto use
 
-# Run build script
-just url="$CF_PAGES_URL" prod
+# Run build script (use normal URL for production build, provided URL for preview build)
+if [[ "$CF_PAGES_BRANCH" == "main" ]]; then
+	just prod
+else
+	just url="$CF_PAGES_URL" prod
+fi
